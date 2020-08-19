135891b8ad69dcd63bafa56cd54191711795dcd8
Chris Sanders <astericky@gmail.com>
2020-08-19 14:23:33 -0700

    add blogroll

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,5 +15,13 @@ module.exports = {
       },
     },
     `gatsby-plugin-netlify-cms`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: `markdown-pages`,
+      }
+    },
+    `gatsby-transformer-remark`,
   ],
 }
