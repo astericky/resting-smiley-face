3e5e4d9e63eac8961c19b53e8039a2a8d3becbff
Chris Sanders <astericky@gmail.com>
2020-08-19 18:03:04 -0700

    add resume page

---
+++ src/pages/resume.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Container from '../components/container'
+
+export default function Resume() {
+  return (
+    <Container>
+      Resume Page Coming!
+    </Container>
+  )
+}
