8019106ab3d528847d0b80f431dbe80432399a42
Chris Sanders <astericky@gmail.com>
2020-08-19 22:44:48 -0700

    update blog post

--- src/components/blogroll.js
+++ src/components/blogroll.js
@@ -1,7 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { graphql, StaticQuery } from 'gatsby'
-// import { Link, graphql, StaticQuery } from 'gatsby'
+import { Link, graphql, StaticQuery } from 'gatsby'
+import styled from 'styled-components'
+
+const BlogRollStyled = styled.section`
+  h3 {
+    margin: 0 0 6px;
+  }
+`
 
 function BlogRoll({ 
   data: { 
@@ -11,15 +17,25 @@ function BlogRoll({
   },
 }) {
   return (
-    <div>
+    <BlogRollStyled>
       { posts &&
         posts.map(({ node: post }) => (
-          <article key={post.id}>
-            {post.frontmatter.title}
+          <article
+            className={`blog-post-card${post.frontmatter.featuredpost ? ' is-featured' : ''}`} 
+            key={post.id}
+          >
+            <header>
+              <h3>
+                <Link to={post.frontmatter.path}>{post.frontmatter.title}</Link>
+              </h3>
+              <div className="blog-post-meta">{post.frontmatter.date}</div>
+            </header>
+            <p>{post.excerpt}</p>
+            <Link to={post.frontmatter.path}>Read More</Link>
           </article>
         ))
       }
-    </div>
+    </BlogRollStyled>
   )
 }
 
