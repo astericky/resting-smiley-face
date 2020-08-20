c3b79f191e25105c9df186bd04e754fede756fe0
Chris Sanders <astericky@gmail.com>
2020-08-19 20:00:53 -0700

    update header styles

--- src/components/header.js
+++ src/components/header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import styled from 'styled-components'
 import { Link } from 'gatsby'
 
+import Nav from '../components/nav'
+
 const HeaderStyle = styled.header`
   display: grid;
   grid-template-columns: auto 1fr auto;
@@ -16,64 +18,25 @@ const LogoStyle = styled.h1`
   margin: 0;
   font-size: 1.25rem;
 
-  @media (min-width: 600px) {
+  a {
+    background: none;
+    text-shadow: none;
+  }
+
+  @media (min-width: 475px) {
     font-size: 2rem;
   }
 `
 
 const Logo = ({ text }) => (
-  <LogoStyle>{text}</LogoStyle>
+  <LogoStyle><Link to="/">{text}</Link></LogoStyle>
 )
-
-const HamburgerIcon = () => (
-  <div className="hamburger-icon">
-    &#9776;
-  </div>
-)
-
-const NavContainer = styled.div`
-  grid-area: nav;
-  align-self: center;
-
-  .hamburger-icon {
-    font-size: 32px;
-  }
-  
-  @media (min-width: 600px) {
-    .hamburger-icon {
-      display: none;
-    }
-  }
-`
-
-const NavStyle = styled.nav`
-  display: none;
-  grid-template-columns: repeat(4, 1fr);
-  grid-column-gap: 12px;
-  align-self: center;
-
-  a {
-    text-align: center;
-  }
-
-  @media (min-width: 600px) {
-    display: inline-grid;
-  }
-`
 
 export default function header() {
   return (
     <HeaderStyle>
       <Logo text="&#128513; Resting Smiley Face" />
-      <NavContainer>
-        <HamburgerIcon />
-        <NavStyle>
-          <Link to="/">Home</Link>
-          <Link to="/resume">Resume</Link>
-          <Link to="/blog">Blog</Link>
-          <Link to="/contact">Contact</Link>
-        </NavStyle>
-      </NavContainer>
+      <Nav />
     </HeaderStyle>
   )
 }
