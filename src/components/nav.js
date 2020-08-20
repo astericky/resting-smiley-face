7d1c45548797e697274f52bd6d849c4f5c5e1ef1
Chris Sanders <astericky@gmail.com>
2020-08-19 20:00:59 -0700

    update header styles

---
+++ src/components/nav.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Link } from 'gatsby'
+
+const NavContainer = styled.div`
+  grid-area: nav;
+  align-self: center;
+
+  .hamburger-icon {
+    font-size: 32px;
+  }
+  
+  @media (min-width: 600px) {
+    .hamburger-icon {
+      display: none;
+    }
+  }
+`
+
+const HamburgerIcon = () => (
+  <div className="hamburger-icon">
+    &#9776;
+  </div>
+)
+
+const NavStyle = styled.nav`
+  display: none;
+  grid-template-columns: repeat(4, 1fr);
+  grid-column-gap: 12px;
+  align-self: center;
+
+  a {
+    text-align: center;
+    text-shadow: none;
+  }
+
+  @media (min-width: 600px) {
+    display: inline-grid;
+  }
+`
+
+export default function Nav() {
+  return (
+    <NavContainer>
+      <HamburgerIcon />
+      <NavStyle>
+        <Link to="/">Home</Link>
+        <Link to="/resume">Resume</Link>
+        <Link to="/blog">Blog</Link>
+        <Link to="/contact">Contact</Link>
+      </NavStyle>
+    </NavContainer>
+  )
+}
