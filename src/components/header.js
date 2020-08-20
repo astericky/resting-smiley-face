caa71bebd727aae747051a7da7a0887d1b6910bb
Chris Sanders <astericky@gmail.com>
2020-08-19 19:32:37 -0700

    update header styles

--- src/components/header.js
+++ src/components/header.js
@@ -12,22 +12,51 @@ const HeaderStyle = styled.header`
 `
 
 const LogoStyle = styled.h1`
+  align-self: center;
   margin: 0;
+  font-size: 1.25rem;
+
+  @media (min-width: 600px) {
+    font-size: 2rem;
+  }
 `
 
 const Logo = ({ text }) => (
   <LogoStyle>{text}</LogoStyle>
 )
 
+const HamburgerIcon = () => (
+  <div className="hamburger-icon">
+    &#9776;
+  </div>
+)
+
+const NavContainer = styled.div`
+  grid-area: nav;
+
+  .hamburger-icon {
+    font-size: 32px;
+  }
+  
+  @media (min-width: 600px) {
+    .hamburger-icon {
+      display: none;
+    }
+  }
+`
+
 const NavStyle = styled.nav`
-  display: inline-grid;
+  display: none;
   grid-template-columns: repeat(4, 1fr);
   grid-column-gap: 12px;
-  grid-area: nav;
   align-self: center;
 
   a {
     text-align: center;
+  }
+
+  @media (min-width: 600px) {
+    display: inline-grid;
   }
 `
 
@@ -35,12 +64,15 @@ export default function header() {
   return (
     <HeaderStyle>
       <Logo text="&#128513; Resting Smiley Face" />
-      <NavStyle>
-        <Link to="/">Home</Link>
-        <Link to="/resume">Resume</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/contact">Contact</Link>
-      </NavStyle>
+      <NavContainer>
+        <HamburgerIcon />
+        <NavStyle>
+          <Link to="/">Home</Link>
+          <Link to="/resume">Resume</Link>
+          <Link to="/blog">Blog</Link>
+          <Link to="/contact">Contact</Link>
+        </NavStyle>
+      </NavContainer>
     </HeaderStyle>
   )
 }
